4bc79850ba8969c74ac387ad09f4cafe9a051a7b
Israel Fonseca <israel.bgf@gmail.com>
2015-02-12 16:14:44 -0200

    visual effects for submitting

--- webflask/static/js/index.js
+++ webflask/static/js/index.js
@@ -1,25 +1,40 @@
 $(function(){
-    $('form').validate({
 
-        submitHandler: function(form) {
-            $('input[type=submit]').fadeOut(function(){
-                $(form).ajaxSubmit({
-                   success: function(){
-                       $('.success-message').fadeIn();
-                   },
-                    error : function(){
-                        $('.error-message').fadeIn();
-                    }
+    var ajaxLoader = setUpAjaxLoader();
+    setUpFormToBeAjaxEnabled();
+
+    function setUpAjaxLoader() {
+        var ajaxLoader = $('<img src="/static/images/ajax-loader.gif" class="ajax-loader"/>');
+        ajaxLoader.hide();
+        ajaxLoader.appendTo($('#confirmation'));
+        return ajaxLoader;
+    }
+
+    function setUpFormToBeAjaxEnabled() {
+        $('form').validate({
+            submitHandler: function (form) {
+                $('input[type=submit]').fadeOut(function () {
+                    ajaxLoader.fadeIn(function () {
+                        $(form).ajaxSubmit({
+                            success: function () {
+                                ajaxLoader.fadeOut(function () {
+                                    $('.success-message').fadeIn();
+                                });
+                            },
+                            error: function () {
+                                ajaxLoader.fadeOut(function () {
+                                    $('.error-message').fadeIn();
+                                });
+                            }
+                        });
+                    });
                 });
-            });
-        },
-
-        errorElement: "span",
-        errorPlacement: function(error, element) {
-            $(element).prev().append(error);
-        },
-
-        errorClass: "error-message"
-        
-    });
+            },
+            errorElement: "span",
+            errorPlacement: function (error, element) {
+                $(element).prev().append(error);
+            },
+            errorClass: "error-message"
+        });
+    }
 });
