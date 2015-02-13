1be374dc80356728457f9db7ada39a66874bc13e
Israel Fonseca <israel.bgf@gmail.com>
2015-02-12 22:39:05 -0200

    logging

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 
-if os.environ.get('DELIVERY_MECHANISM', 'FLASK'):
+if os.environ.get('DELIVERY_MECHANISM', 'FLASK') == 'FLASK':
+    print '****', 'Using FLASK', '*****'
     from webflask.entrypoint import app
 else:
+    print '****', 'Using DJANGO', '*****'
     from webdjango.wsgi import app
 
 if __name__ == "__main__":
