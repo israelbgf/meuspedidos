84546a21fa2ab9cc6e2c538b5322f4efb0b0b94a
Israel Fonseca <israel.bgf@gmail.com>
2015-03-05 16:23:33 -0300

    refactoring

--- webdjango/test.py
+++ webdjango/test.py
@@ -22,3 +22,22 @@ class EmailGateway(TestCase):
         gateway = DjangoEmailGateway()
         gateway.send(self.to_email, self.templates)
         self.assertEqual(len(mail.outbox), 4)
+
+
+# class PersistenceGateway(TestCase):
+#
+#     def setUp(self):
+#         self.to_email = 'juca@dev.com'
+#         self.templates = ['FRONTEND', 'BACKEND', 'MOBILE', 'NONE']
+#
+#     @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+#                        CELERY_ALWAYS_EAGER=True)
+#     def test_async_gateway(self):
+#         gateway = DjangoAsyncEmailGateway()
+#         gateway.send(self.to_email, self.templates)
+#         self.assertEqual(len(mail.outbox), 4)
+#
+#     def test_sync_gateway(self):
+#         gateway = DjangoEmailGateway()
+#         gateway.send(self.to_email, self.templates)
+#         self.assertEqual(len(mail.outbox), 4)
